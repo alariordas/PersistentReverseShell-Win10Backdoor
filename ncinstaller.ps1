9aefa992245c313cfc13ed41af0c8645be74bbea
alariordas <80034759+alariordas@users.noreply.github.com>
2023-09-14 17:51:18 +0200

    Update ncinstaller.ps1

--- ncinstaller.ps1
+++ ncinstaller.ps1
@@ -1,4 +1,4 @@
-function DescargarYDescomprimirNetcat {
+
     # Obtiene el nombre de usuario actual
     $usuarioActual = $env:USERNAME
 
@@ -7,13 +7,13 @@ function DescargarYDescomprimirNetcat {
     $destinationPath = "C:\Users\$usuarioActual\Downloads\netcat-1.11"
 
     # Descargar el archivo ZIP
-    Invoke-WebRequest -Uri "URL_DEL_ARCHIVO_ZIP" -OutFile $zipFilePath
+    Invoke-WebRequest -Uri "https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip" -OutFile $zipFilePath
 
     # Descomprimir el archivo ZIP
     Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath
 
     # UNZIP DEL NC.EXE EN SYSTEM32: Mover el archivo nc.exe a la carpeta System32
-    $ncFilePath = "C:\Users\daw1xx\Downloads\netcat-win32-1.11\nc.exe"
+    $ncFilePath = "C:\Users\alari\Downloads\netcat-1.11\netcat-1.11\nc.exe"
     $system32Path = "C:\Windows\System32"
 
     # Mover nc.exe a la carpeta System32
@@ -26,6 +26,10 @@ function DescargarYDescomprimirNetcat {
     netsh advfirewall firewall add rule name="Service Firewall" dir=in action=allow protocol=TCP localport=455 enable=yes
 
     # BORRAR EL ZIP Y LA CARPETA
-    Remove-Item -Path $ncFilePath -Force
+    $ncPath = "C:\Users\$usuarioActual\Downloads\netcat-1.11"
+    Remove-Item -Path $ncPath -Recurse -Force
     Remove-Item -Path $zipFilePath -Force
-}
+
+    
+    # Al final del script
+    Read-Host -Prompt "Presiona Enter para salir"
